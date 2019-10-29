54e4a75d024c687de1e4d8e89f7d0047527eaab8
Petra Frantz <petra091@gmail.com>
2019-10-29 20:05:51 +0000

    split screen and car controls

--- RacingGame/Control3DProgram.py
+++ RacingGame/Control3DProgram.py
@@ -23,12 +23,13 @@ class GraphicsProgram3D:
 
         self.model_matrix = ModelMatrix()
 
-        self.view_matrix = ViewMatrix()
-        self.view_matrix.look(Point(0, 0, 3), Point(0, 0, 0), Vector(0, 1, 0))
-        self.shader.set_view_matrix(self.view_matrix.get_matrix())
+        self.view_matrix_1 = ViewMatrix()
+        self.view_matrix_2 = ViewMatrix()
+        self.view_matrix_1.look(Point(0, 0, 3), Point(0, 0, 0), Vector(0, 1, 0))
+        self.view_matrix_2.look(Point(3, 0, 0), Point(0, 0, 0), Vector(0, 1, 0))
 
         self.projection_matrix = ProjectionMatrix()
-        self.projection_matrix.set_perspective(pi/2, 800/600, 0.5, 500)
+        self.projection_matrix.set_perspective(pi/3, 800/300, 0.5, 500)
         self.shader.set_projection_matrix(self.projection_matrix.get_matrix())
 
         self.skybox = Skybox()
@@ -52,8 +53,18 @@ class GraphicsProgram3D:
         self.LEFT_key_down = False
         self.RIGHT_key_down = False
 
-        # Walking speed 
-        self.walk_speed = 3
+        # Max speed 
+        self.max_speed = 20
+        self.max_turn = pi / 2
+        # Car speed per second
+        self.speed1 = 0
+        self.speed2 = 0
+        self.turn1 = 0
+        self.turn2 = 0
+        # Turn speed
+        self.turn_speed = 1
+        # Acceleration
+        self.acceleration = 4 
 
         self.texture_id01 = self.load_texture("/textures/spacesky.webp")
         self.texture_id02 = self.load_texture("/textures/box2.png")
@@ -74,38 +85,53 @@ class GraphicsProgram3D:
 
     def update(self):
         delta_time = self.clock.tick() / 1000.0
-
         self.angle += pi * delta_time
+        ########### Car controls 1 ############
+              # go forward
+        if self.UP_key_down:
+            if (self.speed1 < self.max_speed):
+                self.speed1 += self.acceleration * delta_time
+            self.view_matrix_1.slide(0, 0, delta_time * -self.speed1)
+        else:
+            if (self.speed1 > 0):
+                self.speed1 -= self.acceleration * delta_time * 2
+            self.view_matrix_1.slide(0, 0, delta_time * -self.speed1)
+            if (self.speed1 < 0):
+                self.speed1 = 0
 
-        # Slide forward, backward
-        if self.W_key_down:
-            self.view_matrix.slide(0, 0, delta_time * -self.walk_speed)
-        if self.S_key_down:
-            self.view_matrix.slide(0, 0, delta_time * self.walk_speed)
-
-        # Slide left, right
-        if self.D_key_down:
-            self.view_matrix.slide(delta_time * self.walk_speed, 0, 0)
-        if self.A_key_down:
-            self.view_matrix.slide(delta_time * -self.walk_speed, 0, 0)
-
+        # If press right, turn right
         # YAW (rotate camera left and right) 
         if self.LEFT_key_down:
-            self.view_matrix.yaw(-pi * delta_time)
+            self.turn1 = delta_time * -pi * self.speed1/20
+            self.view_matrix_1.yaw(self.turn1 * self.turn_speed)
         if self.RIGHT_key_down:
-            self.view_matrix.yaw(pi * delta_time)
+            self.turn1 = delta_time * pi * self.speed1/20
+            self.view_matrix_1.yaw(self.turn1 * self.turn_speed)
 
-    def display(self):
-        glEnable(GL_DEPTH_TEST) 
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        ########### Car controls 2 ############
+        # go forward
+        if self.W_key_down:
+            if (self.speed2 < self.max_speed):
+                self.speed2 += self.acceleration * delta_time
+            self.view_matrix_2.slide(0, 0, delta_time * -self.speed2)
+        else:
+            if (self.speed2 > 0):
+                self.speed2 -= self.acceleration * delta_time * 2
+            self.view_matrix_2.slide(0, 0, delta_time * -self.speed2)
+            if (self.speed2 < 0):
+                self.speed2 = 0
 
-        glViewport(0, 0, 800, 600)
+        # If press right, turn right
+        # YAW (rotate camera left and right) 
+        if self.A_key_down:
+            self.turn2 = delta_time * -pi * self.speed2/20
+            self.view_matrix_2.yaw(self.turn2 * self.turn_speed)
+        if self.D_key_down:
+            self.turn2 = delta_time * pi * self.speed2/20
+            self.view_matrix_2.yaw(self.turn2 * self.turn_speed)
+        delta_time = self.clock.tick() / 1000.0
 
-        self.shader.set_view_matrix(self.view_matrix.get_matrix())
-        # self.shader.set_eye_position(self.view_matrix.eye)
-        # self.shader.set_light_position(self.view_matrix.eye)
-        # self.shader.set_light_diffuse(1.0, 1.0, 1.0)
-        
+    def displayScreen(self):
         self.shader.set_light_position(Point(4.0, 4.0, 4.0))
         self.shader.set_light_diffuse(1.0, 1.0, 1.0)
         self.shader.set_light_specular(1.0, 1.0, 1.0)
@@ -120,7 +146,7 @@ class GraphicsProgram3D:
         self.shader.set_material_diffuse(1.0, 1.0, 1.0)
         self.shader.set_material_specular(0.0, 0.0, 0.0)
         self.model_matrix.push_matrix()
-        self.model_matrix.add_scale(400,400,400)
+        self.model_matrix.add_scale(600,600,600)
         # self.model_matrix.add_rotate_y(self.angle * 0.5)
         self.shader.set_model_matrix(self.model_matrix.matrix)
         self.skybox.draw(self.shader)
@@ -154,6 +180,18 @@ class GraphicsProgram3D:
         self.cube.draw(self.shader)
         self.model_matrix.pop_matrix()
 
+    def display(self):
+        glEnable(GL_DEPTH_TEST)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        self.shader.set_view_matrix(self.view_matrix_1.get_matrix())
+        glViewport(0, 0, 800, 300)
+        self.displayScreen()
+
+        self.shader.set_view_matrix(self.view_matrix_2.get_matrix())
+        glViewport(0, 300, 800, 300)
+        self.displayScreen()
+        
         pygame.display.flip()
 
     def program_loop(self):
